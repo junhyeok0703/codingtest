bf0caf1eaf64acc2e466ea4f73bbad1f0b7c3dde
jun_code <101074298+junhyeok0703@users.noreply.github.com>
2023-12-01 11:02:12 +0900

    투포인터알고리즘
    투포인터

---
+++ 1253.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string.h>
+#include <algorithm>
+using namespace std;
+
+int main(void){
+
+    int N;
+    cin >> N;
+    
+    int* Array = new int[N];
+
+    for (int i = 0; i < N; ++i) {
+       cin >> Array[i];
+    }
+    sort(Array,Array+N);
+    
+  
+    int k =0;
+    for(int a =2;a<N;a++){
+        k=Array[a];
+        int i = 0;
+        int j = N-1;
+        while(i<j){
+            if(Array[+])
+        }
+    }
+
+
+}
